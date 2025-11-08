ebd79786dccf8397bd116de102a8559c04ab5f16
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-11-08 17:29:03 +0100

    Update flake

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Show template:
+nix flake show github:Sobottasgithub/java-template/main
 
+Use template:
+nix flake init --template github:Sobottasgithub/java-template/main
--- flake.nix
+++ flake.nix
@@ -8,49 +8,37 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        jdk = pkgs.jdk21.override {};
-
-        buildInputs = [
-          jdk
-        ];
-
-        devTools = [
-          pkgs.google-java-format
-        ];
-
+      perSystem = { pkgs, ... }: let
+        jdk = pkgs.jdk21;
+        buildInputs = [ jdk ];
+        devTools = [ pkgs.google-java-format ];
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = buildInputs ++ devTools;
         };
 
-        packages = {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "java-template";
-            version = "1.0.0";
-
-            src = ./.;
-
-            nativeBuildInputs = buildInputs ++ [ pkgs.makeWrapper ];
-                                    
-            buildPhase = ''
-              javac -Werror -g:none -deprecation -verbose Main.java
-            '';
-
-            installPhase = ''
-              mkdir -p $out/{bin,lib}
-              cp *.class $out/lib
-
-              makeWrapper ${pkgs.lib.getExe jdk} $out/bin/java-template --add-flags "-cp $out/lib/ Main"
-            '';
-          };
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "java-template";
+          version = "1.0.0";
+          src = ./.;
+          nativeBuildInputs = buildInputs ++ [ pkgs.makeWrapper ];
+          buildPhase = ''
+            javac -Werror -g:none -deprecation -verbose Main.java
+          '';
+          installPhase = ''
+            mkdir -p $out/{bin,lib}
+            cp *.class $out/lib/
+            makeWrapper ${pkgs.lib.getExe jdk} $out/bin/java-template \
+              --add-flags "-cp $out/lib/ Main"
+          '';
         };
       };
-      flake = {
+
+      templates.default = {
+        description = "Java project template";
+        source = ./.;
       };
     };
 }
