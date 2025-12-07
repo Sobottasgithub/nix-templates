aa538a01015e9551c4dc898db7e3f6ccae2667fe
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-12-07 21:20:36 +0100

    update: add to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,48 +1,54 @@
 {
   description = "A collection of flake templates";
 
-  outputs =
-    { self, ... }:
-    {
+  outputs = { self, ... }: {
 
-      templates = {
-        empty = {
-          path = ./empty;
-          description = "Empty flake";
-        };
-
-        java = {
-          path = ./java;
-          description = "Java template";
-          welcomeText = ''
-            # Getting started
-            - Run `nix run`
-          '';
-        };
-
-        javaFX = {
-          path = ./javaFX;
-          description = "JavaFX template";
-          welcomeText = ''
-            # Getting started
-            - Run `nix run`
-          '';
-        };
-
-        baisc = {
-          path = ./basic;
-          description = "flake utils with devshell";
-          welcomeTest = ''
-            # Enable direnv to enter devshell
-            direnv allow
-          '';
-        };
-      };
-
-      # Optional: a default template
-      templates.default = {
+    templates = {
+      empty = {
         path = ./empty;
         description = "Empty flake";
       };
+
+      java = {
+        path = ./java;
+        description = "Java template";
+        welcomeText = ''
+          # Getting started
+          - Run `nix run`
+        '';
+      };
+
+      javaFX = {
+        path = ./javaFX;
+        description = "JavaFX template";
+        welcomeText = ''
+          # Getting started
+          - Run `nix run`
+        '';
+      };
+
+      baisc = {
+        path = ./basic;
+        description = "flake utils with devshell";
+        welcomeTest = ''
+          # Enable direnv to enter devshell
+          direnv allow
+        '';
+      };
     };
+
+    cpp = {
+      path = ./cpp;
+      description = "Cpp template";
+      welcomeText = ''
+        # Getting started
+        - Run `nix run`
+      '';
+    };
+
+    templates.default = {
+      path = ./empty;
+      description = "Empty flake";
+    };
+  };
 }
