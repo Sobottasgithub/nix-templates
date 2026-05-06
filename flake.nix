d8bf1ba6ca7ea3313161a2954b449e901323cff4
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2026-05-06 08:53:30 +0000

    update: create php template and format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,90 +1,101 @@
 {
   description = "A collection of flake templates";
 
-  outputs = { self, ... }: {
+  outputs =
+    { self, ... }:
+    {
 
-    templates = {
-      empty = {
-        path = ./empty;
-        description = "Empty flake";
-      };
+      templates = {
+        empty = {
+          path = ./empty;
+          description = "Empty flake";
+        };
 
-      java = {
-        path = ./java;
-        description = "Java template";
-        welcomeText = ''
-          # Getting started
-          - Run `nix run`
-        '';
-      };
+        java = {
+          path = ./java;
+          description = "Java template";
+          welcomeText = ''
+            # Getting started
+            - Run `nix run`
+          '';
+        };
 
-      javaFX = {
-        path = ./javaFX;
-        description = "JavaFX template";
-        welcomeText = ''
-          # Getting started
-          - Run `nix run`
-        '';
-      };
+        javaFX = {
+          path = ./javaFX;
+          description = "JavaFX template";
+          welcomeText = ''
+            # Getting started
+            - Run `nix run`
+          '';
+        };
 
-      basic = {
-        path = ./basic;
-        description = "flake utils with devshell";
-        welcomeTest = ''
-          # Enable direnv to enter devshell
-          direnv allow
-        '';
-      };
+        basic = {
+          path = ./basic;
+          description = "flake utils with devshell";
+          welcomeTest = ''
+            # Enable direnv to enter devshell
+            direnv allow
+          '';
+        };
 
-      cpp = {
-        path = ./cpp;
-        description = "Cpp template";
-        welcomeText = ''
-          # Getting started
-          - Run `nix run`
-        '';
-      };
+        cpp = {
+          path = ./cpp;
+          description = "Cpp template";
+          welcomeText = ''
+            # Getting started
+            - Run `nix run`
+          '';
+        };
 
-      cpp-multi = {
-        path = ./cpp-multi;
-        description = "Cpp multi app template";
-        welcomeText = ''
-          # Welcome to your cpp multi application!
-          - Run 'nix run .#A' or 'nix run .#B' to start your experience! 
-        '';
-      };
+        cpp-multi = {
+          path = ./cpp-multi;
+          description = "Cpp multi app template";
+          welcomeText = ''
+            # Welcome to your cpp multi application!
+            - Run 'nix run .#A' or 'nix run .#B' to start your experience! 
+          '';
+        };
 
-      latex = {
-        path = ./latex;
-        description = "Latex template";
-        welcomeText = ''
-          # Have fun with your fully working latex experience!
-        '';
-      };
+        latex = {
+          path = ./latex;
+          description = "Latex template";
+          welcomeText = ''
+            # Have fun with your fully working latex experience!
+          '';
+        };
 
-      python = {
-        path = ./python;
-        description = "Python template";
-        welcomeText = ''
-          # Have fun with your fully working python experience! 
-        '';
-      };
+        python = {
+          path = ./python;
+          description = "Python template";
+          welcomeText = ''
+            # Have fun with your fully working python experience! 
+          '';
+        };
 
-      rust = {
-        path = ./rust;
-        description = "Rust template";
-        welcomeText = ''
-          # Have fun with your fully working python experience!
-          try:
+        rust = {
+          path = ./rust;
+          description = "Rust template";
+          welcomeText = ''
+            # Have fun with your fully working python experience!
+            try:
 
-          nix run
-        '';
-      };
+            nix run
+          '';
+        };
 
-      templates.default = {
-        path = ./empty;
-        description = "Empty flake";
+        php = {
+          path = ./php;
+          description = "PHP template with dev-server";
+          welcomeText = ''
+            # Getting started
+            - Run `nix run` to launch the php development server
+          '';
+        };
+
+        templates.default = {
+          path = ./empty;
+          description = "Empty flake";
+        };
       };
     };
-  };
 }
